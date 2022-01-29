5fe774184e7c335c60f95605fecef6fe669ecbec
msyfls123 <msyfls123@gmail.com>
2022-01-29 17:27:32 +0800

    feat: use spawn_local to execute JsFuture.

--- client/src/helpers/request/graph.rs
+++ client/src/helpers/request/graph.rs
@@ -1,7 +1,7 @@
-use wasm_bindgen::{JsValue, prelude::Closure, JsCast};
-use web_sys::{window, Request, RequestInit, console, Response};
+use wasm_bindgen::{JsValue, JsCast};
+use wasm_bindgen_futures::{spawn_local, JsFuture};
+use web_sys::{window, Request, RequestInit, Response};
 
-// TODO use wasm_bindgen_futures::spawn_local to pipe promise
 pub fn get_vertex_data<F: 'static>(callback: F) where F: FnOnce(JsValue) -> () {
     let win = window().unwrap();
     let mut options = RequestInit::new();
@@ -11,17 +11,11 @@ pub fn get_vertex_data<F: 'static>(callback: F) where F: FnOnce(JsValue) -> () {
     options.headers(&JsValue::from_serde(&headers).unwrap());
     let req = Request::new_with_str_and_init("./vertex", &options).unwrap();
     let fetch_res = win.fetch_with_request(&req);
-    let callback1 = Closure::once(Box::new(|raw_res: JsValue| {
-        console::log_1(&raw_res);
+    spawn_local(async {
+        let raw_res = JsFuture::from(fetch_res).await.unwrap();
         let res: Response = raw_res.dyn_into().unwrap();
-        let json_data = res.json().unwrap();
-        let callback2 = Closure::once(Box::new(|data| {
-            console::log_1(&data);
-            callback(data);
-        }));
-        json_data.then(&callback2);
-        callback2.forget();
-    }));
-    fetch_res.then(&callback1);
-    callback1.forget();
+        let promise_data = res.json().unwrap();
+        let data = JsFuture::from(promise_data).await.unwrap();
+        callback(data);
+    });
 }
